cd0f6e498d14c0a55e9d15b8ea40e5ba76b91e28
Yang Ge <eroicaleo@gmail.com>
2014-04-04 16:32:35 -0700

    Added recursion in BinarySearch

--- ch01/1.1/BinarySearch.java
+++ ch01/1.1/BinarySearch.java
@@ -13,6 +13,17 @@ public class BinarySearch {
 
         return -1;
     }
+    public static int rank_recursion(int key, int[] a) {
+        return rank_recursion(key, a, 0, a.length-1);
+    }
+    public static int rank_recursion(int key, int[] a, int lo, int hi) {
+        if (lo > hi)
+            return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) return rank_recursion(key, a, lo, mid-1);
+        else if (key > a[mid]) return rank_recursion(key, a, mid+1, hi);
+        else return mid;
+    }
 
     public static void main(String[] args) {
         int[] whitelist = In.readInts(args[0]);
@@ -21,7 +32,10 @@ public class BinarySearch {
 
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
-            if (rank(key, whitelist) == -1) {
+            // if (rank(key, whitelist) == -1) {
+            //     StdOut.println(key);
+            // }
+            if (rank_recursion(key, whitelist) == -1) {
                 StdOut.println(key);
             }
         }
