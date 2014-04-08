54663b9c5238ac9efcaef8a7b9827d788ecb56d1
Yang Ge <eroicaleo@gmail.com>
2014-04-07 17:52:59 -0700

    Added DrawSomething code

---
+++ ch01/1.1/DrawSomething.java
@@ -0,0 +1,9 @@
+public class DrawSomething {
+    public static void main(String[] args) {
+        StdDraw.setXscale(-2.0, 2.0);
+        StdDraw.setYscale(-2.0, 2.0);
+        StdDraw.line(1.0, 1.0, 2.0, 2.0);
+        StdDraw.circle(0.0, 0.0, 1.0);
+        StdDraw.square(0.0, 0.0, 1.0);
+    }
+}
