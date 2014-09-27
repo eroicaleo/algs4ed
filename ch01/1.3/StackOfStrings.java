68037a7b1712d3b8dd0963780407623f1b4d8e4f
Yang Ge <eroicaleo@gmail.com>
2014-09-27 02:01:21 -0700

    Added StackOfStrings.java

---
+++ ch01/1.3/StackOfStrings.java
@@ -0,0 +1,9 @@
+public class StackOfStrings {
+    public StackOfStrings();
+    public void push(String item);
+    public String pop();
+    public boolean isEmpty();
+    private int size();
+    public static void main(String[] args) {
+    }
+}
