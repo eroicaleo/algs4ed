1062bcc1cbe544826d901c7b7f1dc80bc69a1b9d
Yang Ge <eroicaleo@gmail.com>
2014-09-28 02:09:31 -0700

    Updated StackOfStrings.java

--- ch01/1.3/StackOfStrings.java
+++ ch01/1.3/StackOfStrings.java
@@ -1,9 +1,24 @@
 public class StackOfStrings {
-    public StackOfStrings();
-    public void push(String item);
-    public String pop();
-    public boolean isEmpty();
-    private int size();
+    public StackOfStrings() {
+    }
+    public void push(String item) {
+        return;
+    }
+    public String pop() {
+        return "";
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+    private int size() {
+        return 0;
+    }
     public static void main(String[] args) {
+        StackOfStrings stack = new StackOfStrings();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("_")) StdOut.print(stack.pop());
+            else               stack.push(s);
+        }
     }
 }
