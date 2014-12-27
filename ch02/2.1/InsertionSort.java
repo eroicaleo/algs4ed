f659b6857bf7e67345616bc334f04d283cb206a3
Yang Ge <eroicaleo@gmail.com>
2014-12-27 00:01:41 -0800

    Make the InsertionSort work

--- ch02/2.1/InsertionSort.java
+++ ch02/2.1/InsertionSort.java
@@ -1,18 +1,27 @@
 public class InsertionSort {
-    private void exch(Comparable[] a, int i, int j) {
+    private static void exch(Comparable[] a, int i, int j) {
         Comparable tmp = a[i];
         a[i] = a[j];
         a[j] = tmp;
     }
-    private boolean less(Comparable a, Comparable b) {
+    private static boolean less(Comparable a, Comparable b) {
         return a.compareTo(b) < 0;
     }
-    public void sort(Comparable[] a) {
+    public static void sort(Comparable[] a) {
         for (int i = 0; i < a.length; i++)
             for (int j = i-1; j >= 0; j--)
-                if (less(a[j], a[j+1]))
-                    exch(a[j], a[j+1]);
+                if (less(a[j+1], a[j]))
+                    exch(a, j, j+1);
     }
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        Double[] a = new Double[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform();
+        }
+        InsertionSort.sort(a);
+        for (int i = 0; i < N; i++) {
+            StdOut.println(a[i]);
+        }
     }
 }
