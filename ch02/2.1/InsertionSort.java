dda2b4e7396515955c4852280734dcff7fce41ae
Yang Ge <eroicaleo@gmail.com>
2015-02-21 01:10:53 -0800

    section 2.1
      Updates InsertionSort and Makefile

--- ch02/2.1/InsertionSort.java
+++ ch02/2.1/InsertionSort.java
@@ -8,10 +8,9 @@ public class InsertionSort {
         return a.compareTo(b) < 0;
     }
     public static void sort(Comparable[] a) {
-        for (int i = 0; i < a.length; i++)
-            for (int j = i-1; j >= 0; j--)
-                if (less(a[j+1], a[j]))
-                    exch(a, j, j+1);
+        for (int i = 1; i < a.length; i++)
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--)
+                 exch(a, j, j-1);
     }
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
