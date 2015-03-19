b9bcc4a1398de7a8c69b112ad64059e7481b4777
Yang Ge <eroicaleo@gmail.com>
2015-03-18 19:24:24 -0700

    Finish the MergeSort

--- ch02/2.2/MergeSort.java
+++ ch02/2.2/MergeSort.java
@@ -11,6 +11,18 @@ public class MergeSort {
         // FIXME
         return true;
     }
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid+1, hi);
+        merge(a, aux, lo, mid, hi);
+        return;
+    }
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length-1);
+    }
     private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
         assert isSorted(a, lo, mid);
         assert isSorted(a, mid+1, hi);
@@ -21,13 +33,13 @@ public class MergeSort {
         int i = lo, j = mid+1;
         for (int k = lo; k <= hi; k++) {
             if (i > mid)
-                a[k] = a[j++];
+                a[k] = aux[j++];
             else if (j > hi)
-                a[k] = a[i++];
-            else if (less(a[j], a[i]))
-                a[k] = a[j++];
+                a[k] = aux[i++];
+            else if (less(aux[j], aux[i]))
+                a[k] = aux[j++];
             else
-                a[k] = a[i++];
+                a[k] = aux[i++];
         }
 
         assert isSorted(a, lo, hi);
@@ -38,7 +50,7 @@ public class MergeSort {
         for (int i = 0; i < N; i++) {
             a[i] = StdRandom.uniform();
         }
-        // MergeSort.sort(a);
+        MergeSort.sort(a);
         for (int i = 0; i < N; i++) {
             StdOut.println(a[i]);
         }
