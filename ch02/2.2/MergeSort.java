9b2076b47165c35b5d4594187e22a1c8197a612c
Yang Ge <eroicaleo@gmail.com>
2015-03-25 01:12:16 -0700

    MergeSort.java updates
      Added one optimization to MergeSort.java

--- ch02/2.2/MergeSort.java
+++ ch02/2.2/MergeSort.java
@@ -14,32 +14,31 @@ public class MergeSort {
     private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
         if (hi <= lo) return;
         int mid = lo + (hi - lo) / 2;
-        sort(a, aux, lo, mid);
-        sort(a, aux, mid+1, hi);
-        merge(a, aux, lo, mid, hi);
+        sort(aux, a, lo, mid);
+        sort(aux, a, mid+1, hi);
+        merge(aux, a, lo, mid, hi);
         return;
     }
     public static void sort(Comparable[] a) {
         Comparable[] aux = new Comparable[a.length];
+        for (int i = 0; i < a.length; i++)
+            aux[i] = a[i];
         sort(a, aux, 0, a.length-1);
     }
     private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
         assert isSorted(a, lo, mid);
         assert isSorted(a, mid+1, hi);
 
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
-        }
         int i = lo, j = mid+1;
         for (int k = lo; k <= hi; k++) {
             if (i > mid)
-                a[k] = aux[j++];
+                aux[k] = a[j++];
             else if (j > hi)
-                a[k] = aux[i++];
-            else if (less(aux[j], aux[i]))
-                a[k] = aux[j++];
+                aux[k] = a[i++];
+            else if (less(a[j], a[i]))
+                aux[k] = a[j++];
             else
-                a[k] = aux[i++];
+                aux[k] = a[i++];
         }
 
         assert isSorted(a, lo, hi);
