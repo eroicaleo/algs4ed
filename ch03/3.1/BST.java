2823a46c14163766e7ea53512f0f4d30d5b7a342
Yang Ge <eroicaleo@gmail.com>
2015-04-23 12:50:28 -0700

    Added BST
      but only get and put operations

---
+++ ch03/3.1/BST.java
@@ -0,0 +1,37 @@
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        public Node(Key key, Value val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public Value get(Key key) {
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0)      x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return       x.val;
+        }
+        return null;
+    }
+
+    public Node put(Node x, Key key, Value val) {
+        if (x == null) return new Node(key, val);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        return x;
+    }
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+}
