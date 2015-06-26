7f261bff414f50170f8cc1cb3d66b6b7b409b673
eroicaleo <yang@Yangs-MacBook-Pro.local>
2015-06-26 04:59:01 -0400

    updates kd-trees assignment

--- ch03/kd-trees/PointSET.java
+++ ch03/kd-trees/PointSET.java
@@ -1,15 +1,15 @@
 public class PointSET {
     private RedBlackBST rbTree;
 
-    // construct an empty set of points 
+    // construct an empty set of points
     public PointSET() {
         rbTree = new RedBlackBST<Point2D, Integer>();
     }
-    // is the set empty? 
+    // is the set empty?
     public boolean isEmpty() {
         return rbTree.isEmpty();
     }
-    // number of points in the set 
+    // number of points in the set
     public int size() {
         return rbTree.size();
     }
@@ -18,19 +18,23 @@ public class PointSET {
         rbTree.put(p, 0);
         return;
     }
-    // does the set contain point p? 
+    // does the set contain point p?
     public boolean contains(Point2D p) {
         return rbTree.contains(p);
     }
-    // draw all points to standard draw 
+    // draw all points to standard draw
     public void draw() {
         Iterable<Point2D> points = rbTree.keys();
         for (Point2D p : points) {
             StdDraw.point(p.x(), p.y());
         }
     }
-    // public Iterable<Point2D> range(RectHV rect)             // all points that are inside the rectangle 
-    // a nearest neighbor in the set to point p; null if the set is empty 
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return rbTree.keys();
+    }
+
+    // // a nearest neighbor in the set to point p; null if the set is empty
     // public           Point2D nearest(Point2D p) {
     // }
     public static void main(String[] args) {
